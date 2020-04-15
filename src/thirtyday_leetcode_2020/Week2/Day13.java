ee4964ebf93ad9f08c764f50797972bb933b828d
Andrew Kim <commanderjacks@gmail.com>
2020-04-15 01:49:06 -0700

    Day14

---
+++ src/thirtyday_leetcode_2020/Week2/Day13.java
@@ -0,0 +1,23 @@
+package thirtyday_leetcode_2020.Week2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+// equivalent to LeetCode525
+public class Day13 {
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,0,0};
+        int[] arr2 = {0,1};
+        int[] arr3 = {1,0};
+        int[] arr4 = {1,0,1,0,0,0,0,1};
+        int[] arr5 = {1,1,0,0,0,1,1,1,0};
+        System.out.println("c: " + findMaxLength(arr4));
+        System.out.println("d: " + findMaxLength(arr5));
+    }
+
+    public static int findMaxLength(int[] nums) {
+        return -1;
+    }
+
+}
