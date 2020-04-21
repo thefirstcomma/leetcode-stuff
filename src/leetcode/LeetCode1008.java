d22da886f30dfc0af592ca5b026e7b709286ac05
comma <commanderjacks@gmail.com>
2020-04-20 19:20:53 -0700

    Day20 of 30 Day challenge

---
+++ src/leetcode/LeetCode1008.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+public class LeetCode1008 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {8,5,1,7,10,12};
+        TreeNode as = bstFromPreorder(nums);
+        printPre(as);
+    }
+
+    public static void printPre(TreeNode tn) {
+        if(tn == null) return;
+        System.out.print(tn.val + ", ");
+        printPre(tn.left);
+        printPre(tn.right);
+    }
+
+    public static TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder.length == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        TreeNode head = root;
+
+        for (int i = 1; i < preorder.length; i++) {
+            head = root;
+            while(true) {
+                if(head.val > preorder[i]) {
+                    if(head.left == null) {
+                        head.left = new TreeNode(preorder[i]);
+                        break;
+                    } else {
+                        head = head.left;
+                    }
+                } else {
+                    if(head.right == null) {
+                        head.right = new TreeNode(preorder[i]);
+                        break;
+                    } else {
+                        head = head.right;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+}
