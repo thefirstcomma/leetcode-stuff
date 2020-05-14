4eec1ac7b65d03effe18d56e84ceb524f891e473
Andrew Kim <andrewkim260@gmail.com>
2020-05-13 21:11:41 -0700

    Remote suspension for intraconnected networks

---
+++ src/may_thirtyday_leetcode_2020/week2/Day13.java
@@ -0,0 +1,11 @@
+package may_thirtyday_leetcode_2020.week2;
+
+public class Day13 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String removeKdigits(String num, int k) {
+        return null;
+    }
+}
