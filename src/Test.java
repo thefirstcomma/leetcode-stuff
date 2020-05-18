4076b58f222394e89ac8738da4212c178f233a46
comma <andrewkim260@gmail.com>
2020-05-17 20:49:41 -0700

    Day 17 of LeetCode 30 Day challenge in May

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,60 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
+        List<Integer> list = slidingWindowTemplate("aabbbbbacbc", "abc");
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
 
+    public static List<Integer> slidingWindowTemplate(String s, String t) {
+        //init a collection or int value to save the result according the question.
+        List<Integer> list = new LinkedList<>();
+        if(t.length()> s.length()) return list;
+
+        //create a hashmap to save the Characters of the target substring.
+        //(K, V) = (Character, Frequence of the Characters)
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        //maintain a counter to check whether match the target string.
+        //must be the map size, NOT the string size because the char may be duplicate.
+        int counter = map.size();
+        int head = 0;
+        int begin = 0, end = 0;
+
+        //the length of the substring which match the target string.
+        int len = Integer.MAX_VALUE;
+
+        //loop at the begining of the source string
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)-1);// plus or minus one
+                if(map.get(c) == 0) counter--;//modify the counter according the requirement(different condition).
+            }
+            end++;
+
+            //increase begin pointer to make it invalid/valid again
+            /* counter condition. different question may have different condition */
+            while(counter == 0){
+                char ch = s.charAt(begin);//***be careful here: choose the char at begin pointer, NOT the end pointer
+                if(map.containsKey(ch)){
+                    map.put(ch,map.get(ch) + 1); //plus or minus one
+                    if(map.get(ch) > 0)
+                        counter++; //modify the counter according the requirement(different condition).
+                }
+
+                /* save / update(min/max) the result if find a target*/
+                // result collections or result int value
+                if(end - begin == t.length()) {
+                    list.add(begin);
+                }
+
+                begin++;
+            }
+        }
+        return list;
     }
 }
---
+++ src/leetcode/LeetCode438.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.*;
+
+public class LeetCode438 {
+
+    public static void main(String[] args) {
+        List<Integer> list = findAnagrams("cbaebabacd", "abc");
+        System.out.println(list.toString());
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new LinkedList<>();
+        if(p.length() > s.length() || s.length() == 0 || p.length() == 0) return list;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : p.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int counter = map.size();
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0)
+                    counter--;
+            }
+            end++;
+
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0)
+                        counter++;
+                }
+                if(end - begin == p.length()) {
+                    list.add(begin);
+                }
+                begin++;
+            }
+        }
+        return list;
+    }
+}
