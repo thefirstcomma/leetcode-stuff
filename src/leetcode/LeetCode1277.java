9606e0dfc75587f6d4c420bf98dc5571ecf765df
comma <andrewkim260@gmail.com>
2020-05-22 19:26:41 -0700

    LeetCode 1277

--- src/leetcode/LeetCode1277.java
+++ src/leetcode/LeetCode1277.java
@@ -1,4 +1,37 @@
 package leetcode;
 
 public class LeetCode1277 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,0,1,0,1},
+                {1,1,1,1,1},
+                {0,1,1,1,1},
+                {0,1,1,1,1},
+                {1,1,0,1,1},
+        };
+        System.out.println(countSquares(matrix));
+        print(matrix);
+    }
+
+    public static void print(int[][] matrix) {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int countSquares(int[][] matrix) {
+        int counter = 0;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(i > 0 && j > 0 && matrix[i][j] > 0) {
+                    matrix[i][j] = Math.min(matrix[i-1][j-1], Math.min(matrix[i-1][j], matrix[i][j-1])) + 1;
+                }
+                counter += matrix[i][j];
+            }
+        }
+        return counter;
+    }
 }
--- src/may_thirtyday_leetcode_2020/week3/Day21.java
+++ src/may_thirtyday_leetcode_2020/week3/Day21.java
@@ -4,16 +4,33 @@ package may_thirtyday_leetcode_2020.week3;
 public class Day21 {
 
     public static void main(String[] args) {
+        int[][] matrix = {{1,0,1,0,1},
+                        {1,1,1,1,1},
+                        {0,1,1,1,1},
+                        {0,1,1,1,1},
+                        {1,1,0,1,1},
+        };
+        System.out.println(countSquares(matrix));
+        print(matrix);
+    }
 
+    public static void print(int[][] matrix) {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + ", ");
+            }
+            System.out.println();
+        }
     }
 
     public static int countSquares(int[][] matrix) {
-        int[] indicies = new int[matrix.length];
         int counter = 0;
         for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                if(matrix[i][j] == 1)
-                    counter++;
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(i > 0 && j > 0 && matrix[i][j] > 0) {
+                    matrix[i][j] = Math.min(matrix[i-1][j-1], Math.min(matrix[i-1][j], matrix[i][j-1])) + 1;
+                }
+                counter += matrix[i][j];
             }
         }
         return counter;
